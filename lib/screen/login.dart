42a69cde941e899747d6419716f83464a9cfeb0c
Dewadewa <warasbanget76@gmail.com>
2024-02-02 13:56:32 +0700

    first commit

---
+++ lib/screen/login.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Login extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameContoller = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          SizedBox(
+              height: height / 5, child: Image.asset('images/BgLogin.png')),
+          ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 15,
+                  left: 50,
+                  right: 50,
+                  top: 140,
+                ),
+                child: Container(
+                  height: height / 40,
+                  child: Image.asset('images/TextLogin.png'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 5,
+                  left: 50,
+                  right: 50,
+                  top: 5,
+                ),
+                child: Container(
+                  height: height / 3,
+                  child: Image.asset('images/LoginImage.png'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 35,
+                  right: 35,
+                  top: 35,
+                ),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 15,
+                          ),
+                          child: TextFormField(
+                            controller: _usernameContoller,
+                            cursorColor: Colors.black,
+                            decoration: InputDecoration(
+                                hintText: 'Enter username',
+                                hintStyle: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.black,
+                                ),
+                                fillColor:
+                                    const Color(0xffFFFFFF).withOpacity(0.4),
+                                filled: true,
+                                contentPadding: const EdgeInsets.fromLTRB(
+                                    20.0,
+                                    16.0,
+                                    20.0,
+                                    16.0), // Adjust left and right padding as neededcontentPadding: EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 16.0), // Adjust left and right padding as needed
+                                border: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                      color: Color(0xffFF0000),
+                                      width: 2.0,
+                                    ),
+                                    borderRadius: BorderRadius.circular(35))),
+                          ),
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          cursorColor: Colors.black,
+                          decoration: InputDecoration(
+                              hintText: 'Enter password',
+                              hintStyle: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
+                              fillColor:
+                                  const Color(0xffFFFFFF).withOpacity(0.4),
+                              filled: true,
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  20.0,
+                                  16.0,
+                                  20.0,
+                                  16.0), // Adjust left and right padding as needed
+                              border: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Color(0xffFF0000),
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(35))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 70,
+                          ),
+                          child: Container(
+                            width: width,
+                            decoration: BoxDecoration(
+                                color: const Color(0xffFF0000),
+                                borderRadius: BorderRadius.circular(35)),
+                            child: const Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                // onTap: () {
+                                //   Navigator.push(
+                                //       context,
+                                //       MaterialPageRoute(
+                                //           builder: (context) => Bnb()));
+                                // },
+                                child: Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: Text(
+                                    'Login',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xffFFFFFF),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/splash.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:haru_warehouse/screen/login.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Login()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xffFF0000), Color(0xffFFD4D4)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: height / 1, child: Image.asset("images/logo.png")),
+          ],
+        ),
+      ),
+    );
+  }
+}
